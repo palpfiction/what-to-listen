c190649a18dd46bfa1099b76eac78cc3f6483fad
Pablo <pablomunozgarcia@outlook.com>
2026-05-10 17:42:08 +0200

    only log to stdout, no file

--- src/server/index.js
+++ src/server/index.js
@@ -1,45 +1,35 @@
-require('dotenv').config()
+require("dotenv").config();
 
-const express = require('express')
-const port = process.env.PORT || 5000
-const cors = require('cors')
-const path = require('path')
-const fs = require('fs')
-const morgan = require('morgan')
-const WhatToListen = require('./WhatToListen')
+const express = require("express");
+const port = process.env.PORT || 5000;
+const cors = require("cors");
+const path = require("path");
+const fs = require("fs");
+const morgan = require("morgan");
+const WhatToListen = require("./WhatToListen");
 
-var logStream = fs.createWriteStream(path.join(__dirname, '../../server.log'), { flags: 'a' })
+const app = express();
+app.disable("x-powered-by");
+const whatToListen = new WhatToListen(process.env.API_KEY);
 
+app.use(cors());
+app.use(morgan("tiny"));
+app.use(express.static(path.join(__dirname, "../../public")));
 
-const app = express()
-app.disable('x-powered-by');
-const whatToListen = new WhatToListen(process.env.API_KEY)
+app.get("/get-album", async (req, res) => {
+  if (!req.query.user) return res.status(400).send("username is required");
 
-app.use(cors())
-app.use(morgan('tiny',  { stream: logStream }));
-app.use(express.static(path.join(__dirname, '../../public')));
+  const result = await whatToListen.getAlbumToListen({
+    user: req.query.user,
+    minPlayCount: parseInt(req.query.minPlayCount),
+    maxPlayCount: parseInt(req.query.maxPlayCount),
+    period: req.query.period,
+  });
 
-app.get('/get-album', async (req, res) => {
-	if (!req.query.user) return res.status(400).send("username is required");
-
-	const result = await whatToListen.getAlbumToListen({
-		user: req.query.user,
-		minPlayCount: parseInt(req.query.minPlayCount),
-		maxPlayCount: parseInt(req.query.maxPlayCount),
-		period: req.query.period
-	})
-
-	if (result.error) res.status(500)
-	res.send(result)
-})
+  if (result.error) res.status(500);
+  res.send(result);
+});
 
 app.listen(port, () => {
-	console.info(`
-	▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄
-	█░███░█░████░▄▄▀█▄░▄███▄░▄█▀▄▄▀███░███▄██░▄▄█▄░▄█░▄▄█░▄▄▀█▄░█
-	█▄▀░▀▄█░▄▄░█░▀▀░██░█████░██░██░███░███░▄█▄▄▀██░██░▄▄█░██░█░▄█
-	██▄█▄██▄██▄█▄██▄██▄█████▄███▄▄████▄▄█▄▄▄█▄▄▄██▄██▄▄▄█▄██▄█▀██
-	▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀
-	`)
-	console.info(`Server is listening for requests at port ${port}`)
-})
+  console.info(`Server is listening for requests at port ${port}`);
+});
